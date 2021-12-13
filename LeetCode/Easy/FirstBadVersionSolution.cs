292e51e3652d5d12148c09dd90043766d4187ac6
rahajustone <rahajustone>
2021-12-12 23:11:39 -0600

    278. First Bad Version

---
+++ LeetCode/Easy/FirstBadVersionSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class FirstBadVersionSolution
+    {
+        public static bool IsBadVersion(int m)
+        {
+            return true;
+        }
+        public static int FirstBadVersion(int n)
+        {
+            int start = 0;
+            int finish = n;
+            int ans = -1;
+
+            while (start <= finish)
+            {
+                int m = start + ((finish - start) / 2);
+                if (IsBadVersion(m))
+                {
+                    finish = m - 1;
+                    ans = m;
+                }
+                else
+                    start = m + 1;
+            }
+
+            return ans;
+        }
+    }
+}
