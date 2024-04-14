d48b75ed6b75fc4d31d036567860be2670bf3dfd
rahajustone <kh.rahmatullo@gmail.com>
2024-04-14 10:53:36 -0400

    add BestTimeBuyAndSellStock solution

--- LeetCode/Easy/BestTimeBuyAndSellStock.cs
+++ LeetCode/Easy/BestTimeBuyAndSellStock.cs
@@ -3,10 +3,25 @@ namespace LeetCode.Easy
 {
 	public class BestTimeBuyAndSellStock
 	{
-        //public static int MaxProfit(int[] prices)
-        //{
+        public int MaxProfitHardCoded(int[] prices)
+        {
+            int prevProfit = 0;
+            int Maxprofit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                    if (prices[i] < prices[j])
+                    {
+                        prevProfit = prices[j] - prices[i];
+                        if (prevProfit > Maxprofit)
+                        {
+                            Maxprofit = prevProfit;
+                        }
+                    }
+            }
 
-        //}
+            return Maxprofit;
+        }
     }
 }
 
