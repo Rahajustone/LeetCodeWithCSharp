bc4d69ede87edcca26e50344928ec7e62fd3e137
rahajustone <kh.rahmatullo@gmail.com>
2024-05-25 20:41:37 -0400

    Add stack question and solution

---
+++ LeetCode/Easy/MaximumNestingDepthParentheses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace LeetCode.Easy
+{
+	public class MaximumNestingDepthParentheses
+	{
+        public int MaxDepth(string s)
+        {
+
+            Stack<char> chars = new Stack<char>();
+            int count = 0;
+            int max = int.MinValue;
+
+            // "(1+(2*3)+((8)/4))+1"
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    chars.Push(s[i]);
+                    count++;
+                }
+                if (s[i] == ')')
+                {
+                    chars.Pop();
+                    count--;
+                }
+
+                if (max < count) max = count;
+            }
+
+            return count;
+        }
+
+        public int MaxDepthTwoPointer(string s)
+        {
+
+            Stack<char> chars = new Stack<char>();
+            int count = 0;
+            int max = int.MinValue;
+
+            // "(1+(2*3)+((8)/4))+1"
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    chars.Push(s[i]);
+                    count++;
+                }
+                if (s[i] == ')')
+                {
+                    chars.Pop();
+                    count--;
+                }
+
+                if (max < count) max = count;
+            }
+
+            return count;
+        }
+    }
+}
+
---
+++ LeetCode/Medium/FourSumSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class FourSumSolution
+	{
+       // TODO
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            var result = new List<IList<int>>();
+            if (nums.Length < 4) return result;
+
+            Array.Sort(nums);
+
+            int start = 0, left = 1, right = nums.Length - 1;
+
+            int minDistance = int.MaxValue;
+            int sum = int.MinValue;
+
+            while (start < nums.Length-2)
+            {
+                int tempValue = target - nums[start];
+                while (left<right)
+                {
+                    int currentSum = tempValue - (nums[left] + nums[right]);
+                    if(currentSum == target)
+                    {
+                        var temp = new List<int>();
+                    }
+
+
+                }
+
+
+            }
+
+            return result;
+        }
+    }
+}
+
