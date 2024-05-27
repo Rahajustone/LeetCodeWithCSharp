4055037b3d0bdec9939c09d67a528745cf668292
rahajustone <kh.rahmatullo@gmail.com>
2024-05-27 10:57:13 -0400

    MaximumTwinSumOfALinkedLisit

---
+++ LeetCode/Medium/BuildAnArrayWithStackOperations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class BuildAnArrayWithStackOperations
+	{
+		public BuildAnArrayWithStackOperations()
+		{
+		}
+
+
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            IList<string> result = new List<string>();
+
+            int index = 0;
+            for (int i = 1; i <= n && index < target.Length; i++)
+            {
+                if (i == target[index])
+                {
+                    result.Add("Push");
+                    index++;
+                }
+                else
+                {
+                    result.Add("Push");
+                    result.Add("Pop");
+                }
+            }
+
+            return result;
+        }
+
+
+        public IList<string> BuildArrayFirsWay(int[] target, int n)
+        {
+            Stack<int> streamStack = new Stack<int>();
+            Stack<string> targetStack = new Stack<string>();
+            IList<string> result = new List<string>();
+            for (int i = 1; i <= target[target.Length - 1]; i++)
+            {
+                streamStack.Push(i);
+            }
+
+            int targetIndex = target.Length - 1;
+            while (streamStack.Count>0)
+            {
+                if(streamStack.Peek() == target[targetIndex])
+                {
+                    streamStack.Pop();
+                    targetStack.Push("Push");
+                    targetIndex--;
+                } else
+                {
+                    streamStack.Pop();
+                    targetStack.Push("Pop");
+                    targetStack.Push("Push");
+                }
+            }
+
+            while(targetStack.Count>0) {
+                result.Add(targetStack.Pop());
+            }
+
+            return result;
+        }
+    }
+}
+
--- LeetCode/Medium/MaximumTwinSumOfALinkedList.cs
+++ LeetCode/Medium/MaximumTwinSumOfALinkedList.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 namespace LeetCode.Medium
 {
+ 
 	public class MaximumTwinSumOfALinkedList
 	{
 		public MaximumTwinSumOfALinkedList()
 		{
 		}
-	}
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public int PairSum(ListNode head)
+        {
+			var slow = head;
+			var fast = head;
+			Stack<int> first = new Stack<int>();
+            int max = int.MinValue;
+
+			while (fast != null && fast.next != null)
+			{
+                first.Push(slow.val);
+                fast = fast.next.next;
+                slow = slow.next;
+			}
+
+            while(slow != null)
+            {
+                var tempMin = slow.val + first.Pop();
+                if (tempMin > max) max = tempMin;
+                slow = slow.next;
+            }
+
+            return max;
+        }
+    }
 }
 
