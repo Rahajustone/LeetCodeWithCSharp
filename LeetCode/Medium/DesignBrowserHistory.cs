4c66e666e4d6d2076b8013cc9dc68934be7f2fb2
rahajustone <kh.rahmatullo@gmail.com>
2024-05-28 10:21:52 -0400

    Add Browser history

---
+++ LeetCode/Medium/DesignBrowserHistory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class DesignBrowserHistory
+	{
+        Stack<string> forwardSteps;
+        Stack<string> backwardSteps;
+        private int stepsCount = 0;
+
+        public DesignBrowserHistory(string homepage)
+        {
+            forwardSteps = new Stack<string>();
+            backwardSteps = new Stack<string>();
+            forwardSteps.Push(homepage);
+        }
+
+        public void Visit(string url)
+        {
+            forwardSteps.Push(url);
+
+            // clears up all forward history
+            while(backwardSteps.Count>0)
+            {
+                backwardSteps.Pop();
+            }
+        }
+
+        public string Back(int steps)
+        {
+            while (steps-- > 0 && forwardSteps.Count > 1)
+            {
+                backwardSteps.Push(forwardSteps.Pop());
+            }
+
+            return forwardSteps.Peek();
+        }
+
+        public string Forward(int steps)
+        {
+            while (steps-- >0 && backwardSteps.Count > 0)
+            {
+                forwardSteps.Push(backwardSteps.Pop());
+            }
+
+            return forwardSteps.Peek();
+        }
+    }
+}
+
