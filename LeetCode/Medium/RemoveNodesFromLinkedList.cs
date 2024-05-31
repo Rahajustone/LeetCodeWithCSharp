5cc9e390f21f772117f0b2ca5d9292711507c6a8
rahajustone <kh.rahmatullo@gmail.com>
2024-05-31 10:35:42 -0400

    RemoveNodesFromLinkedList

---
+++ LeetCode/Medium/RemoveNodesFromLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class RemoveNodesFromLinkedList
+	{
+		public RemoveNodesFromLinkedList()
+		{
+		}
+
+        public ListNode RemoveNodes(ListNode head)
+        {
+
+            ListNode newNode = null;
+            Stack<int> st = new Stack<int>();
+
+            var current = head;
+            while (current != null)
+            {
+
+                while (st.Count > 0 && st.Peek() < current.val)
+                {
+                    st.Pop();
+                }
+                st.Push(current.val);
+
+                current = current.next;
+            }
+
+            while (st.Count > 0)
+            {
+                if (newNode == null)
+                {
+                    newNode = new ListNode(st.Pop());
+                }
+                else
+                {
+                    var temp = newNode;
+                    newNode = new ListNode(st.Pop());
+                    newNode.next = temp;
+                }
+            }
+            return newNode;
+        }
+    }
+}
+
