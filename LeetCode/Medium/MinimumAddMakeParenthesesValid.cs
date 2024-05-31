49895a4dcbe6627034b743063af871661d0c290c
rahajustone <kh.rahmatullo@gmail.com>
2024-05-31 10:03:58 -0400

    Add Stack solved question

---
+++ LeetCode/Medium/MinimumAddMakeParenthesesValid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class MinimumAddMakeParenthesesValid
+	{
+        public int MinAddToMakeValid(string s)
+        {
+            Stack<char> paranthes = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    paranthes.Push(s[i]);
+                }
+                else if (s[i] == ')')
+                {
+                    if (paranthes.Count > 0 && paranthes.Peek() == '(')
+                    {
+                        paranthes.Pop();
+                    }
+                    else
+                    {
+                        paranthes.Push(s[i]);
+                    }
+                }
+            }
+
+            return paranthes.Count;
+        }
+    }
+}
+
