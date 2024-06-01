e27395edce51cb79c91bd5524c3ee968420dbedd
rahajustone <kh.rahmatullo@gmail.com>
2024-06-01 11:42:32 -0400

    ValidateStackSequencesSolution

---
+++ LeetCode/Medium/MinimumNumberSwaps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LeetCode.Medium;
+
+public class MinimumNumberSwaps
+{
+	public MinimumNumberSwaps()
+	{
+	}
+
+// [[][]]
+// ]][[[]
+    public int MinSwaps(string s)
+    {
+	Stack<char> chars = new Stack<char>();
+	int countLeft = 0; //]
+	foreach (var c in s)
+	{
+		if (c == '[') chars.Push(c);
+		else if (chars.Count > 0) chars.Pop();
+		else countLeft++;
+	}
+
+	int braces = Math.Max(chars.Count, countLeft);
+
+	return braces%2==0 ? braces / 2 : (braces + 1) / 2; 
+    }
+}
---
+++ LeetCode/Medium/ValidateStackSequences.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LeetCode.Medium
+{
+	public class ValidateStackSequences
+	{
+		public ValidateStackSequences()
+		{
+		}
+
+        // Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+        // Output: true
+		public bool ValidateStackSequencesSolution(int[] pushed, int[] popped)
+		{
+			int poppedLength = popped.Length;
+			int pushedLength = pushed.Length;
+
+            if (poppedLength != pushedLength) return false;
+            if (pushedLength > 1000) return false;
+
+			Stack<int> nums = new Stack<int>();
+			int poppedIndex = 0;
+			int pushedIndex = 0;
+
+			while(poppedIndex< poppedLength)
+			{
+				if(nums.Any() && nums.Peek() == popped[poppedIndex])
+				{
+					nums.Pop();
+					poppedIndex++;
+					continue;
+				}
+
+				if(pushedIndex< pushedLength)
+				{
+					nums.Push(pushed[pushedIndex++]);
+					continue;
+				}
+
+				return false;
+			}
+
+			return true;
+        }
+	}
+}
+
