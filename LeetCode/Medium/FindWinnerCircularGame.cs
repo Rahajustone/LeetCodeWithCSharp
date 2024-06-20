0b24c9bef35e2056ff7a8c180383edee0ec54911
rahajustone <kh.rahmatullo@gmail.com>
2024-06-19 21:16:55 -0400

    1823. Find the Winner of the Circular Game

---
+++ LeetCode/Medium/FindWinnerCircularGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium
+{
+    internal class FindWinnerCircularGame
+    {
+        public int FindTheWinner(int n, int k)
+        {
+            Queue<int> integers = new Queue<int>(Enumerable.Range(1, n));
+
+            int indexCount = 1;
+            while (integers.Count > 1) {
+                if (indexCount == k)
+                {
+                    integers.Dequeue();
+                    indexCount = 1;
+                }
+                else
+                {
+                    indexCount++;
+                    integers.Enqueue(integers.Dequeue());
+                }
+            }
+
+            return integers.Dequeue(); ;
+        }
+    }
+}
---
+++ LeetCode/Medium/RevealCardsInIncreasingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium
+{
+    internal class RevealCardsInIncreasingOrder
+    {
+        public int[] DeckRevealedIncreasing(int[] deck)
+        {
+            Array.Sort(deck);
+            Queue<int> queueDeck = new Queue<int>(Enumerable.Range(0, deck.Length));
+            int[] result = new int[deck.Length];
+
+            foreach (var card in deck)
+            {
+                result[queueDeck.Dequeue()] = card;
+                if(queueDeck.Count>0)
+                    queueDeck.Enqueue(queueDeck.Dequeue());
+            }
+
+            return result;
+        }
+    }
+}
