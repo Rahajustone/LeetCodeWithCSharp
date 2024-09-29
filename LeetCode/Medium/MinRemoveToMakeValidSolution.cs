f4e475a144864d376bb7c7659ec2acbaf13b4925
rahajustone <kh.rahmatullo@gmail.com>
2024-09-29 16:46:17 -0400

    Fix

---
+++ LeetCode/Medium/MinRemoveToMakeValidSolution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+	public class MinRemoveToMakeValidSolution
+	{
+		public MinRemoveToMakeValidSolution()
+		{
+		}
+
+  //      Input: s = "lee(t(c)o)de)"
+		//Output: "lee(t(c)o)de"
+		//Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+		//Example 2:
+
+		//Input: s = "a)b(c)d"
+		//Output: "ab(c)d"
+		//Example 3:
+
+		//Input: s = "))(("
+
+        public string MinRemoveToMakeValid(string s)
+		{
+			Stack<int> integers = new Stack<int>();
+			StringBuilder str = new StringBuilder(s);
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (s[i] == '(')
+				{
+					integers.Push(i);
+				} else
+				{
+					if(integers.Count>0)
+					{
+						integers.Pop();
+					} else
+					{
+						str[i] = '*';
+					}
+				}
+			}
+
+			while (integers.Count > 0)
+			{
+				str[integers.Pop()] = '*';
+			}
+
+            StringBuilder finalResult = new StringBuilder();
+            foreach (char c in str.ToString())
+            {
+                if (c != '*')
+                    finalResult.Append(c);
+            }
+
+            return finalResult.ToString();
+        }
+	}
+}
+
---
+++ LeetCode/Medium/ReverseParenthesesSolution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+	public class ReverseParenthesesSolution
+	{
+		public ReverseParenthesesSolution()
+		{
+		}
+
+        public string ReverseParentheses(string s)
+        {
+            Stack<char> reverseStr = new Stack<char>();
+            StringBuilder str = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (c != ')')
+                {
+                    reverseStr.Push(c);
+                    continue;
+                }
+
+                StringBuilder strTemp = new StringBuilder();
+                while (reverseStr.Count > 0 && c == ')')
+                {
+                    if (reverseStr.Peek() == '(')
+                    {
+                        reverseStr.Pop();
+                        break;
+                    }
+                    else
+                    {
+                        var t = reverseStr.Pop();
+                        strTemp.Append(t);
+                    }
+                }
+
+
+                for (int i = 0; i < strTemp.Length; i++)
+                {
+                    reverseStr.Push(strTemp[i]);
+                }
+            }
+
+            var newStack = new Stack<char>();
+
+            while (reverseStr.Count > 0)
+            {
+                newStack.Push(reverseStr.Pop());
+            }
+
+            while (newStack.Count > 0)
+            {
+                str.Append(newStack.Pop());
+            }
+
+            return str.ToString();
+        }
+    }
+}
+
